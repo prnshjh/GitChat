b22ff691d832e385406649ebaf5d5290a089735b
jhapriyanshu19 <prnshjh@gmail.com>
2025-05-23 19:14:31 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div><h1 className='text-amber-500'> Hello world
+      </h1>
+      <Button>Click Me</Button></div>
+  )
+}
+
+export default page
